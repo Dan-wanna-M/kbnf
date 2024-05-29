d6c479b6ccce956d1a1de3e714bb2380edf54ee0
Huanghe <xs28@rice.edu>
2024-05-29 13:58:56 -0500

    Fix bug for tokens containing 0xFF

--- kbnf/src/vocabulary.rs
+++ kbnf/src/vocabulary.rs
@@ -6,7 +6,6 @@ use std::array;
 use tinyvec::ArrayVec;
 
 const TOKEN_SEPARATOR: u8 = 0xFF;
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Token(pub Box<[u8]>);
 #[derive(Debug, Clone)]
@@ -17,10 +16,14 @@ pub struct Vocabulary {
     id_to_token: Vec<Token>,
     /// This field represents a map from token id to the token in UTF-8 String representation.
     id_to_token_string: Vec<String>,
-    /// This field represents a map from the first byte of a token to the token id and token.
+    /// This field represents a map from the first byte of a token to the token id and token that DO NOT contain byte 0xFF.
     /// memory representation: [Unicode-unused-byte][token_id(3bytes little endian)][token(remaining bytes)]
-    // TODO: support better debugg display
-    first_byte_to_tokens: JaggedArray<u8, ArrayVec<[u8; 256]>, 2>,
+    // TODO: support better debug display
+    // TODO: check whether a variable length token_id encoding is better
+    first_byte_to_normal_tokens: JaggedArray<u8, ArrayVec<[usize; 256]>, 2>,
+    /// This field represents a map from the token id to the token that contains byte 0xFF.
+    /// The number of such tokens is expected to be small so we probably do not need a jagged array(which does have some overhead).
+    tokens_containing_separators: Vec<(u32, Token)>,
 }
 
 impl Vocabulary {
@@ -30,9 +33,9 @@ impl Vocabulary {
         id_to_token_string: Vec<String>,
     ) -> Self {
         assert!(
-            id_to_token.len() < 0x1000000 - 1,
+            id_to_token.len() < 0x1000000,
             "max token id is larger than 2^24: {}",
-            id_to_token.len()
+            id_to_token.len()-1
         );
         let mut first_byte_to_token = JaggedArray::with_capacity([256, 256]);
         let mut temp: [Vec<(u32, &Token)>; 256] = array::from_fn(|_| (vec![]));
@@ -40,10 +43,15 @@ impl Vocabulary {
             let first_byte = token.0[0];
             temp[first_byte as usize].push((token_id as u32, token));
         }
+        let mut tokens_containing_separators = Vec::new();
         for tokens in temp.iter() {
             first_byte_to_token.new_row::<0>();
-            for (token_id, token) in tokens.iter() {
+            for &(token_id, token) in tokens.iter() {
                 let mut buffer = vec![TOKEN_SEPARATOR];
+                if token.0.contains(&TOKEN_SEPARATOR) {
+                    tokens_containing_separators.push((token_id, token.clone()));
+                    continue;
+                }
                 buffer.extend(token_id.to_le_bytes().into_iter().take(3));
                 buffer.extend(token.0.iter());
                 first_byte_to_token.extend_last_row(buffer.into_iter());
@@ -53,7 +61,8 @@ impl Vocabulary {
             token_to_id,
             id_to_token,
             id_to_token_string,
-            first_byte_to_tokens: first_byte_to_token,
+            first_byte_to_normal_tokens: first_byte_to_token,
+            tokens_containing_separators,
         }
     }
 
@@ -75,15 +84,19 @@ impl Vocabulary {
         self.id_to_token.len()
     }
 
-    pub(crate) fn get_tokens_from_first_byte(&self, first_byte: u8) -> TokensIter {
+    pub(crate) fn get_normal_tokens_from_first_byte(&self, first_byte: u8) -> TokensIter {
         TokensIter {
             current_token_id: None,
             iter: self
-                .first_byte_to_tokens
+                .first_byte_to_normal_tokens
                 .view::<1, 1>([first_byte as usize])
                 .as_slice()
                 .iter(),
         }
+    }
+
+    pub(crate) fn get_tokens_containing_separators(&self) -> impl Iterator<Item = (u32, &Token)> {
+        self.tokens_containing_separators.iter().map(|(x, y)| (*x, y))
     }
 }
 
