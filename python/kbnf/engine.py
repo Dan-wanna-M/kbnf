81310a5c0ddd88f72494354795b0a2a0ed130107
Huanghe <xs28@rice.edu>
2024-09-17 14:43:52 -0500

    Make logits actually pinned in memory

--- python/kbnf/engine.py
+++ python/kbnf/engine.py
@@ -51,16 +51,13 @@ def _torch_fast_mask_logits(module:types.ModuleType):
                 length = num_of_disallowed
                 if length == 0: # Rust FFI requires non-null pointer
                     return tensor
-                disallowed = module.empty((length,), device="cpu",dtype=module.int64)
-                if tensor.is_cuda:
-                    disallowed.pin_memory()
+                pinned = tensor.is_cuda # only pin if the logits is on CUDA, which implies the user is using CUDA for its LLM
+                disallowed = module.empty((length,), device="cpu",dtype=module.int64, pinned=pinned)
                 data_ptr = disallowed.data_ptr()
                 assert data_ptr % 8 == 0, f"The indices data pointer which points to {data_ptr} is not aligned to 8 bytes"
                 engine.write_disallowed_token_ids_to_buffer(data_ptr, length)
                 length = engine.get_number_of_allowed_token_ids()
-                allowed = module.empty((length,), device="cpu",dtype=module.int64)
-                if tensor.is_cuda:
-                    allowed.pin_memory()
+                allowed = module.empty((length,), device="cpu",dtype=module.int64, pinned=pinned)
                 data_ptr = allowed.data_ptr()
                 assert data_ptr % 8 == 0, f"The allowed data pointer which points to {data_ptr} is not aligned to 8 bytes"
                 engine.write_allowed_token_ids_to_buffer(data_ptr, length)
